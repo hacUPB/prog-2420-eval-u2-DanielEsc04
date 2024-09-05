f55b108a325d255be12f74c05797ed9d1a15f34b
Daniel <daniel.arangoesc@upb.edu.co>
2024-09-04 19:34:47 -0500

    Codigo satelite

--- src/satelite.py
+++ src/satelite.py
@@ -1,7 +1,23 @@
-
+import time
 def main():
-    #Tu código va aquí
-    pass # borra esta línea cuando con inicies tu código
+    Altitud = float(input("Ingrese la altitud del satelite: "))
+    Altitud_Minima = float(input("Ingrese la altitud minima de seguridad: "))
+    Cd = float(input("Ingrese el coeficiente de arrastre del satelite: "))
+    Altitud_Perdida = 0 
+    Orbitas = 0
+    if Cd < 0.00001:
+        print("Satelite estable")
+    while Altitud > Altitud_Minima and Cd > 0:
+        Altitud_Perdida = Altitud*Cd
+        Altitud = Altitud - Altitud_Perdida
+        Orbitas = Orbitas + 1
+        Cd = Cd + 0.0001
+        print(f'En la orbita #{Orbitas} la altitud del satelite es: {Altitud}')
+        time.sleep(0.25)
+        
+    if Altitud < Altitud_Minima:
+        print(f"Se ha perdido conexión con satelite despues de {Orbitas} orbitas")
+    pass
 
 
 if __name__ == "__main__":
